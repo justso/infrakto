f83c06d7b27869238dfb718fa4f7e0de87abba41
dvdrtrgn <dvdrtrgn@gmail.com>
2012-10-25 18:05:55 -0500

    doc and tidy

--- js/plax.js
+++ js/plax.js
@@ -1,70 +1,75 @@
-(function($){
+(function ($) {
+    var ready, $document, $win;
+    //  scrollPlax global PRIVATE STATICS
+    function gStat() {
+        if (ready) {
+            return;
+        }
+        $document = glob.doc;
+        $win = glob.win;
+    }
     $.scrollPlax = function (box, options) {
+        //  instance PRIVATE STATICS
         options = options || {};
+        gStat();
         var $box
-        ,   $document = glob.doc
-        ,   $window = glob.win
-        ,   backgroundMode = false
-        ,   boxHeight
+        ,   bkgrMode = false
         ,   boxWidth
-        ,   docHeight
         ,   docWidth
-        ,   isIE6 = ($.browser.msie && $.browser.version < 7 ? true : false)
         ,   loopCount = 0
-        ,   loopCountX = 0
-        ,   maxIE6Move = 0
-        ,   maxIE6MoveX = 0
-        ,   oldMoveIt = null
-        ,   oldMoveItX = null
+        ,   startPos = 0
         ,   plaxRoom
-        ,   plaxRoomX
-        ,   startingPos = 0
-        ,   startingPosX = 0
-        ,   tooSmallMode = false
-        ,   tooSmallModeX = false
-        ,   windowHeight
+        ,   tinyMode = false
         ,   windowWidth
+        ,   oldMoveIt = null
         ;
         options.staticSpeed = options.staticSpeed || false;
-        options.staticScrollLimit = typeof(options.staticScrollLimit) !== 'undefined' ? options.staticScrollLimit : false;
+        options.scrollLimit = typeof(options.scrollLimit) !== 'undefined' ? options.scrollLimit : false;
         options.loopIt = options.loopIt || false;
-        options.reverseDirection = options.reverseDirection || false;
+        options.revDir = options.revDir || false;
         init(box);
-        // init( obj/string box )   :  sets up the plax and associated events
-
+        /**
+         *  init
+         *  sets up the plax and associated events
+         *
+         *  @param box {obj/string}
+         *  if string append image as bkgr, otherwise define as jQuery object
+         *  @return {void}
+         */
         function init(box) {
-            // if string append image as background, otherwise define as jQuery object
             if (typeof(box) === 'string') {
-                $box = appendBackground(box);
+                $box = appendBkgr(box);
             } else {
                 $box = $(box);
-                $box.css('position', isIE6 ? 'absolute' : 'fixed');
-                startingPos = parseInt($box.css('left'), 10);
-            }
-            if (options.disableIE6Anim && isIE6) {
-                return false;
+                $box.css('position', 'fixed');
+                startPos = parseInt($box.css('left'), 10);
             }
             defineSizes();
-            // if in background mode, and reverseDirection, then attch the background to the opposite end to maximize scrolling room
-            if (backgroundMode) {
-                if (options.reverseDirection) {
-                    startingPos += - 1 * plaxRoom;
-                    $box.css('left', startingPos);
+            // attach bkgr to opposite end to maximize scrolling room?
+            if (bkgrMode) {   // if in bkgr mode
+                if (options.revDir) { // and revDir
+                    startPos += - 1 * plaxRoom;
+                    $box.css('left', startPos);
                 }
             }
             // attach scroll and resize events
-            $window.scroll(function () {
+            $win.scroll(function () {
                 ani();
             });
-            $window.resize(function () {
+            $win.resize(function () {
                 defineSizes();
             });
         }
-        // appendBackground( string theSrc )  :   appends an image to the page as a stretched background
-
-        function appendBackground(theSrc) {
-            var $obj
-            ,   bgCss = {
+        /**
+         *  appendBkgr
+         *  appends an image to the page as a stretched bkgr
+         *
+         *  @param url {string} url for image
+         *  @return {object} for options
+         */
+        function appendBkgr(url) {
+            var $obj, bgCss;
+            bgCss = {
                 display: 'block',
                 top: 0,
                 left: 0,
@@ -73,133 +78,134 @@
                 zIndex: 0
             };
             if (options.bgRepeat) {
-                $obj = options.appendInFront ? $('<div></div>').appendTo($('body')) : $('<div></div>').prependTo($('body'));
-                bgCss.backgroundRepeat = 'repeat';
-                bgCss.backgroundImage = 'url("' + theSrc + '")';
+                $obj = options.appendInFront ? $('<div>').appendTo('body') : $('<div>').prependTo('body');
+                bgCss.bkgrRepeat = 'repeat';
+                bgCss.bkgrImage = 'url("' + url + '")';
             } else {
-                $obj = options.appendInFront ? $('<img>').appendTo($('body')) : $('<img>').prependTo($('body'));
-                $obj.attr('src', theSrc);
+                $obj = options.appendInFront ? $('<img>').appendTo('body') : $('<img>').prependTo('body');
+                $obj.attr('src', url);
             }
             $obj.css(bgCss);
-            backgroundMode = true;
+            bkgrMode = true;
             return $obj;
         }
-        // defineSizes()  :  sets up various constants used by the app - must be set on page load and on screen resize
+        /**
+         *  defineSizes
+         *  set various constants used by the app
+         *  - must be set on page load and on screen resize
+         *
+         *  @return {void}
+         */
         function defineSizes() {
             $('body').css({
                 width: window.innerWidth + 'px',
                 height: window.innerHeight + 'px',
             });
             boxWidth = $box.width();
-            windowWidth = $window.width();
+            windowWidth = $win.width();
             docWidth = $document.width();
             plaxRoom = (options.plaxWidth || boxWidth) - windowWidth;
-            // if plax object is smaller than window size
+            // is object is smaller than window size
             if (plaxRoom < 0) {
                 if (options.staticSpeed) {
                     plaxRoom = windowWidth - boxWidth;
                 } else {
-                    plaxRoom = options.reverseDirection ? windowWidth - startingPos - boxWidth : startingPos;
+                    plaxRoom = options.revDir ? windowWidth - startPos - boxWidth : startPos;
                 }
-                tooSmallMode = true;
-            }
-            if (isIE6 && !maxIE6Move) {
-                maxIE6Move = -1 * (docWidth - boxWidth);
+                tinyMode = true;
             }
             if (options.loopIt) {
-                loopCount = parseInt(($document.scrollTop() / (tooSmallMode ? windowWidth : boxWidth)), 10);
+                loopCount = parseInt(($document.scrollTop() / (tinyMode ? windowWidth : boxWidth)), 10);
             }
-            // make any changes
             ani();
         }
-        // ani()  :  performs the animation of the object
+        /**
+         *  ani
+         *  performs the animation of the object
+         *
+         *  @return {void}
+         */
         function ani() {
-            // dont let multiple animations queue up
-            $box.queue([]);
-            var theCss = {}, moveIt, moveItX;
+            var theCss = {}
+            ,   moveIt
+            ;
             moveIt = calculateMove(true);
             theCss.left = moveIt;
-            // if large move animate in FF, safari and opera for smoother transition
-            if (!$.browser.msie && (Math.abs(oldMoveIt - moveIt) > 100 || Math.abs(oldMoveItX - moveItX) > 100)) {
-                $box.animate(theCss, 30);
+            $box.queue([]); // dont let multiple animations queue up
+
+            if (!$.browser.msie && (Math.abs(oldMoveIt - moveIt) > 10)) {
+                $box.animate(theCss, 30); // animate big jumps in good browsers
             } else {
                 $box.css(theCss);
             }
             oldMoveIt = moveIt;
-            oldMoveItX = moveItX;
         }
-        // calculateMove( boolean vertical ) : determines amount to move whether vertically or horizontally
+        /**
+         *  calculateMove
+         *  determines amount to move whether vertically or horizontally
+         *
+         *  @return {number} move amount
+         */
         function calculateMove() {
-            // establish variables, this is basically a switch between vertical and horizontal modes
-            var boxSize
-            ,   docSize
-            ,   loopCount2
-            ,   maxIE6Move2
-            ,   move
-            ,   offset
-            ,   offsetPercent
-            ,   opts
-            ,   plaxRoom2
-            ,   startingPos2
-            ,   tooSmallMode2
-            ,   windowSize
+            var boxSize = boxWidth
+            ,   docSize = docWidth
+            ,   loopCount2 = loopCount
+            ,   offset = $win.scrlit()
+            ,   plaxRoom2 = plaxRoom
+            ,   startPos2 = startPos
+            ,   tinyMode2 = tinyMode
+            ,   windowSize = windowWidth
+            ,   move, offsetPercent, opts
             ;
-            boxSize = boxWidth;
-            docSize = docWidth;
-            loopCount2 = loopCount;
-            maxIE6Move2 = maxIE6Move;
-            offset = $window.scrlit();
-            plaxRoom2 = plaxRoom;
-            startingPos2 = startingPos;
-            tooSmallMode2 = tooSmallMode;
-            windowSize = windowWidth;
             opts = {
-                reverseDirection: options.reverseDirection,
+                revDir: options.revDir,
                 staticSpeed: options.staticSpeed,
                 loopIt: options.loopIt,
-                staticScrollLimit: options.staticScrollLimit
+                scrollLimit: options.scrollLimit
             };
             if (opts.staticSpeed) {
-                /*** get move amount - static speed ***/
-                move = offset * opts.staticSpeed;
-                // account for number of loops
-                move -= plaxRoom2 * (loopCount2);
+                move = offset * opts.staticSpeed;   // get move amount - static speed
+                move -= (plaxRoom2 * loopCount2);   // account for # of loops
             } else {
-                /*** get move amount - auto speed ***/
-                // determine percentage of page that has been scrolled down
-                offsetPercent = offset / (docSize - windowSize);
-                move = offsetPercent * plaxRoom2;
+                offsetPercent = offset / (docSize - windowSize); // get move amount - auto speed
+                move = offsetPercent * plaxRoom2;   // percentage of page scrolled
             }
-            // reverse direction
-            if (!opts.reverseDirection) {
-                move *= - 1;
+            if (!opts.revDir) {
+                move *= - 1; // reverse direction
             }
             // incorporate starting position
-            move += startingPos2;
+            move += startPos2;
             // if static speed set, make sure move is within bounds
             if (opts.staticSpeed) {
-                move = checkMove(move, opts, plaxRoom2, tooSmallMode2);
+                move = checkMove(move, opts, plaxRoom2, tinyMode2);
             }
-            // if in tooSmallMode and looping, add difference of window height and box height,
-            // since the box needs to be conceptualized as that much taller
-            // (otherwise it would loop in place rather than over the screen)
-            if (tooSmallMode2 && opts.staticSpeed && opts.loopIt) {
-                move += windowSize - boxSize;
+            /*
+                if in tinyMode and looping,
+                add difference of window height and box height,
+                since the box needs to be conceptualized as that much taller
+                (otherwise it would loop in place rather than over the screen)
+             */
+            if (tinyMode2 && opts.staticSpeed && opts.loopIt) {
+                move += (windowSize - boxSize);
             }
-            if (isIE6) {
-                // IE6 fix for fixed positioning
-                move += offset;
-                move = Math.max(parseInt(move, 10), parseInt(maxIE6Move2, 10));
-            }
-            return move|0;
+            return move | 0;
         }
-        // checkMove( int moveIt )  :  checks to ensure that move amount does not exceed established bounds
-        function checkMove(move, opts, plaxRoom, tooSmallMode) {
+        /**
+         *  checkMove
+         *  ensure that move amount does not exceed established bounds
+         *
+         *  @param move {number} pixels?
+         *  @param opts {object} ad-hoc configf from calc
+         *  @param plaxRoom {number} pixels?
+         *  @param tinyMode {boolean} ?
+         *  @return {number} move amount
+         */
+        function checkMove(move, opts, plaxRoom, tinyMode) {
             var loopCountChange;
             // if overflow limited
             if (!opts.loopIt) {
-                if (opts.staticScrollLimit) {
-                    if (tooSmallMode) {
+                if (opts.scrollLimit) {
+                    if (tinyMode) {
                         if (move < 0) {
                             move = 0;
                         }
@@ -215,16 +221,15 @@
                         }
                     }
                 }
-            } else {
-                // if overflow loops
+            } else { // if overflow loops
                 while (move < plaxRoom) {
                     move += plaxRoom;
-                    loopCountChange = (opts.reverseDirection ? - 1 : 1);
+                    loopCountChange = (opts.revDir ? - 1 : 1);
                     loopCount += loopCountChange;
                 }
-                while (move > (opts.reverseDirection ? - 1 : 0)) {
+                while (move > (opts.revDir ? - 1 : 0)) {
                     move -= plaxRoom;
-                    loopCountChange = (opts.reverseDirection ? - 1 : 1);
+                    loopCountChange = (opts.revDir ? - 1 : 1);
                     loopCount -= loopCountChange;
                 }
             }
@@ -233,7 +238,7 @@
     };
     $.fn.scrollPlax = function (options) {
         this.each(function () {
-            var tmp = new $.scrollPlax(this, options);
+            $.scrollPlax(this, options);
         });
         return this;
     };
